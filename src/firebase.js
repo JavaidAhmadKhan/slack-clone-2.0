6e3060ec529edfa9c456e1ecb3e266af2538e7a2
JavaidAhmadKhan <relaxjavaid@gmail.com>
2021-03-10 19:52:50 +0530

    restructuring code files

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC9Zv236dbt-TXu8XUAZHlx4YbhF9U6q3E",
+  authDomain: "slack-clone-df68e.firebaseapp.com",
+  projectId: "slack-clone-df68e",
+  storageBucket: "slack-clone-df68e.appspot.com",
+  messagingSenderId: "787492573038",
+  appId: "1:787492573038:web:496eb695a3876acb0abb7f",
+  measurementId: "G-LXSZ2QFWME",
+};
